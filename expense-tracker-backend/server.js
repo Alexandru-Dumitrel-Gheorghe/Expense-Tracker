59310fb894986ad55b689b5f25c4c796d84dbf5f
Alexandru-Dumitrel-Gheorghe <167351297+Alexandru-Dumitrel-Gheorghe@users.noreply.github.com>
2024-08-29 13:17:24 +0200

    Update MongoDB URI and other configurations

--- expense-tracker-backend/server.js
+++ expense-tracker-backend/server.js
@@ -15,9 +15,11 @@ const app = express();
 
 // Middleware
 app.use(express.json());
+
+// Configure CORS
 app.use(
   cors({
-    origin: "http://localhost:3000",
+    origin: ["http://localhost:3000", "https://numele-tau-de-domeniu.com"], // Permite originile frontend-ului
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"],
   })
@@ -28,14 +30,16 @@ const authRoutes = require("./routes/authRoutes");
 const budgetRoutes = require("./routes/budgetRoutes");
 const transactionRoutes = require("./routes/transactionRoutes");
 const reportRoutes = require("./routes/reportRoutes");
-const todoRoutes = require("./routes/todoRoutes"); // Add this line
+const todoRoutes = require("./routes/todoRoutes");
 
 app.use("/auth", authRoutes);
 app.use("/budgets", budgetRoutes);
 app.use("/transactions", transactionRoutes);
 app.use("/reports", reportRoutes);
-app.use("/todos", todoRoutes); // Add this line
+app.use("/todos", todoRoutes);
 
 // Start the server
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
+module.exports = app;
